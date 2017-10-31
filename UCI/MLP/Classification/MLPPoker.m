8c1b1580addc0ee4c7ef6ace80fad5c5cb9875f4
jrepifano <jrepifano@gmail.com>
2017-10-31 08:25:54 -0400

    Fixed UCI Neural nets

--- UCI/MLP/Classification/MLPPoker.m
+++ UCI/MLP/Classification/MLPPoker.m
@@ -1,12 +1,26 @@
 clc; clear all;
 
 table = readtable("C:\Users\Jake\Documents\MATLAB\MachineLearning\UCI\poker.csv");
-X = table2array(table(:,1:10));
+X = table2array(table(1:500,1:10));
 
-labels = table2array(table(:,11));
+labels = table2array(table(1:500,11));
+labels = convertlabels(labels);
 
-mlp = feedforwardnet(10);
+mlp = feedforwardnet(20);
 net = train(mlp,X',labels');
-view(net)
+%view(net)
 y = net(X');
-perf = perform(net,labels,y);
+perf = perform(net,labels',y);
+
+plotconfusion(labels', y, 'Classifications and Missclassifications');
+
+
+
+function x = convertlabels(labels)
+flabels = zeros(500,10);
+for i = 1:length(labels)
+   y = labels(i)+1;
+   flabels(i,y) = flabels(i,y)+1;
+end
+x = flabels;
+end
--- UCI/MLP/Classification/MLPWine.m
+++ UCI/MLP/Classification/MLPWine.m
@@ -4,9 +4,21 @@ table = readtable("C:\Users\Jake\Documents\MATLAB\MachineLearning\UCI\wine.csv")
 X = table2array(table(:,2:14));
 
 labels = table2array(table(:,1));
+labels = convertlabels(labels);
 
 mlp = feedforwardnet(10);
 net = train(mlp,X',labels');
-view(net)
 y = net(X');
-perf = perform(net,labels,y);
+perf = perform(net,labels',y);
+
+plotconfusion(labels', y, 'Classifications and Missclassifications');
+
+function x = convertlabels(labels)
+flabels = zeros(178,3);
+for i = 1:length(labels)
+   y = labels(i);
+   flabels(i,y) = flabels(i,y)+1;
+end
+x = flabels;
+end
+
