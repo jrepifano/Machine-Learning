570cbb8eba8317930910bbda2bcbdb9da5c5bacf
jrepifano <jrepifano@gmail.com>
2017-12-10 20:46:57 -0500

    fixed formatting issues. Need to re-run on desktop

--- GeneSequencingSOM/V2/FormatData.m
+++ GeneSequencingSOM/V2/FormatData.m
@@ -2,15 +2,23 @@ clc; clear all;
 
 tic
 [Header, Sequence] = fastaread('RDPFull.fasta');
+Sequence = regexprep(Sequence,'N','');
 
 data = [];
 
+k = 8;
 
 for i = 1:length(Sequence)
-   temp = char(Sequence(i));
-   Nmers = nmercount(temp,3);
-   mat = cell2mat(Nmers(:,2));
-   data = horzcat(data,mat);
+    
+       temp = char(Sequence(i));
+       Nmers = nmercount(temp,k);
+       mat = cell2mat(Nmers(:,2));
+       if(length(mat) < 4^k)
+            x = 4^k-length(mat);
+            x = zeros(x,1);
+            mat = [mat;x];
+       end
+       data = [data mat];
     
 end
 toc
