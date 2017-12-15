9cf64b94d7af31fd7c23c7d8662dfffafb119b30
jrepifano <jrepifano@gmail.com>
2017-12-15 01:48:17 -0500

    ghi

--- GeneSequencingSOM/V1/SOMAnalysis.m
+++ GeneSequencingSOM/V1/SOMAnalysis.m
@@ -6,22 +6,18 @@ cat = double(cat);
 
 mouse = load('mouse.mat');
 mouse = getfield(mouse,'intData');
-mouse = mouse(:,1:21297);
 mouse = double(mouse);
 
 dog = load('dog.mat');
 dog = getfield(dog,'intData');
-dog = dog(:,1:21297);
 dog = double(dog);
 
 anolelizard = load('anolelizard.mat');
 anolelizard = getfield(anolelizard,'intData');
-anolelizard = anolelizard(:,1:21297);
 anolelizard = double(anolelizard);
 
 platypus = load('platypus.mat');
 platypus = getfield(platypus,'intData');
-platypus = platypus(:,1:21297);
 platypus = double(platypus);
 
 data = [mouse;dog;cat;anolelizard;platypus];
--- GeneSequencingSOM/V2/SOM.m
+++ GeneSequencingSOM/V2/SOM.m
@@ -1,12 +1,50 @@
 clc; clear all;
 
-data = load('k4.mat');
-data = data.data;
+% tic
+% 
+% labels = load('labels.mat');
+% labels = labels.labels;
+% labels = convertlabels(labels);
+% 
+% performance = [];
+k = [2;4;6;8];
 
-net = selforgmap([2 2],0,5,'gridtop','linkdist');
+for i = 1:4
+    
+data = load(['k'...
+    num2str(k(i)) '.mat']);
+data = data.data;
+       
+net = selforgmap([20 20],0,5,'gridtop','linkdist');
 net.trainParam.epochs=5;
 net = train(net,data);
 y = net(data);
 plotsompos(net,data);
 
-plotconfusion(y,y);
+
+% CP = classperf(labels', y);
+% 
+% x = CP.specificity;
+% performance = [performance;x];
+end
+
+toc
+
+function x = convertlabels(labels)
+flabels = zeros(10678, 2);
+for i = 1:length(labels)
+   y = labels(i);
+   flabels(i,y) = flabels(i,y)+1;
+end
+x = flabels;
+end
+
+% 
+% performance = load('performance.mat');
+% performance = performance.performance;
+% performance = performance*100;
+% 
+% bar(k,performance);
+% title('Classification of Bacteria vs Archaea');
+% xlabel('Number of K-mers');
+% ylabel('Performance');
